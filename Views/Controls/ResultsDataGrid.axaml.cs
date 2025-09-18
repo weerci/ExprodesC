5ee896e956933dd613eafa97373fa7317b2daf3a
weereci <weerci@yandex.ru>
2025-09-18 21:48:30 +0700

    +++

---
+++ Views/Controls/ResultsDataGrid.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ExprodesC.Views.Controls;
+
+public partial class ResultsDataGrid : UserControl
+{
+    public ResultsDataGrid()
+    {
+        InitializeComponent();
+    }
+}
